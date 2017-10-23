acb51748cb3ab52ef8047e3008d1567ff98e4322
JCH <jean-charles.hardy@estaca.eu>
2017-10-23 20:47:58 +0200

    Add ModificationEntities module

---
+++ examples/ModificationEntities.py
@@ -0,0 +1,15 @@
+# coding: utf8
+
+from core.ParsingEntities import Entity
+
+
+class ModificationEntity(Entity):
+    pass
+
+
+class ModificationCondition(ModificationEntity):
+    pass
+
+
+class ModificationOperator(ModificationEntity):
+    pass
