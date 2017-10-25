1b213eb69dece8518d9f40b87339081b8a21a88a
JCH <jean-charles.hardy@estaca.eu>
2017-10-26 00:36:14 +0200

    Refactor ModificationEntities module

--- core/ModificationEntities.py
+++ core/ModificationEntities.py
@@ -1,23 +1,27 @@
 # coding: utf8
 
-from core.ParsingEntities import Entity
 from enum import Enum
+from core.IO import ParsingResult
 
 import abc
 
 
 class ModificationSide(Enum):
-    LEFT = 0
+    LEFT = -1
     RIGHT = 1
 
 
-class ModificationEntity(Entity):
+class ModificationEntity(metaclass=abc.ABCMeta):
     def __add__(self, other):
         if isinstance(self, ModificationEntity) and isinstance(other, ModificationEntity):
-            modification_operator = ModificationOperator()
+            modification_operator = ModificationOperator(self, other)
             return modification_operator
         else:
             raise TypeError("Operands have to be ModificationEntity's subclasses")
+
+    @abc.abstractmethod
+    def generate_parsing_result(self, initial_parsing_result):
+        pass
 
 
 class ModificationOperator(ModificationEntity):
@@ -25,7 +29,7 @@ class ModificationOperator(ModificationEntity):
         self.operandA = operand_a
         self.operandB = operand_b
 
-    def check(self, element, ref_position):
+    def generate_parsing_result(self, initial_parsing_result):
         pass
 
 
@@ -34,10 +38,6 @@ class ModificationOperation(ModificationEntity, metaclass=abc.ABCMeta):
     def __init__(self, rel_position=0):
         self.rel_position = rel_position
 
-    @abc.abstractmethod
-    def check(self, element, ref_position=0):
-        pass
-
 
 class ModificationAdd(ModificationOperation):
     def __init__(self, character, rel_position=0, modification_side=ModificationSide.RIGHT):
@@ -45,13 +45,27 @@ class ModificationAdd(ModificationOperation):
         self.character = character
         self.modificationSide = modification_side
 
-    def check(self, element, ref_position=0):
-        pass
+    def generate_parsing_result(self, initial_parsing_result):
+        ar_index = list()
+        for element in initial_parsing_result:
+            ar_index.append((element[0] + self.rel_position + self.modificationSide.value, self.character))
+        parsing_result = ParsingResult(initial_parsing_result.parsingClass, initial_parsing_result.arInput['args'],
+                                       initial_parsing_result.arInput['kwargs'],
+                                       initial_parsing_result.initialCharacterIndex,
+                                       initial_parsing_result.finalCharacterIndex, ar_index)
+        return parsing_result
 
 
 class ModificationRemove(ModificationOperation):
     def __init__(self, rel_position=0):
         super(ModificationAdd, self).__init__(rel_position=rel_position)
 
-    def check(self, element, ref_position=0):
-        pass
+    def generate_parsing_result(self, initial_parsing_result):
+        ar_index = list()
+        for element in initial_parsing_result:
+            ar_index.append((element[0]+self.rel_position, ''))
+        parsing_result = ParsingResult(initial_parsing_result.parsingClass, initial_parsing_result.arInput['args'],
+                                       initial_parsing_result.arInput['kwargs'],
+                                       initial_parsing_result.initialCharacterIndex,
+                                       initial_parsing_result.finalCharacterIndex, ar_index)
+        return parsing_result
