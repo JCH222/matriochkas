a95c0892e032571451bbc0047241e2f9536466de
JCH <jean-charles.hardy@estaca.eu>
2017-10-30 15:25:53 +0100

    Implement OperatorType unit test

---
+++ tests/ParsingEntities.py
@@ -0,0 +1,17 @@
+# coding: utf8
+
+from core import ParsingEntities
+
+
+def test_operator_type():
+
+    assert len(ParsingEntities.OperatorType) == 3
+
+    assert ParsingEntities.OperatorType.AND.name == 'AND'
+    assert ParsingEntities.OperatorType.AND.value == 'and'
+
+    assert ParsingEntities.OperatorType.OR.name == 'OR'
+    assert ParsingEntities.OperatorType.OR.value == 'or'
+
+    assert ParsingEntities.OperatorType.XOR.name == 'XOR'
+    assert ParsingEntities.OperatorType.XOR.value == 'xor'
