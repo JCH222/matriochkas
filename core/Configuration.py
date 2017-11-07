b051cb5c69eccb50040cb385969222b6f1034d17
JCH <jean-charles.hardy@estaca.eu>
2017-11-07 14:35:52 +0100

    Add key in StreamClassConfiguration

--- core/Configuration.py
+++ core/Configuration.py
@@ -4,5 +4,5 @@ from enum import Enum
 
 
 class StreamClassConfiguration(Enum):
-    StringIO = {'read_method': 'read', 'write_method': 'write', 'return_method': 'getvalue'}
-    TextIOWrapper = {'read_method': 'read', 'write_method': 'write', 'return_method': 'None'}
+    StringIO = {'read_method': 'read', 'write_method': 'write', 'return_method': 'getvalue', 'close_method': 'close'}
+    TextIOWrapper = {'read_method': 'read', 'write_method': 'write', 'return_method': 'None', 'close_method': 'close'}
