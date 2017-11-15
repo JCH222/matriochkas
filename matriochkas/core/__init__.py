82e0550828820eba27bd1d4d8e4c53e01ac9ac4a
JCH <jean-charles.hardy@estaca.eu>
2017-11-15 21:50:11 +0100

    Add core/__init__.py doc

---
+++ matriochkas/core/__init__.py
@@ -0,0 +1,14 @@
+# coding: utf8
+
+
+"""
+    Functional package
+    ==================
+
+    It contains 4 functional modules:
+
+    - Configuration.py
+    - IO.py
+    - ModificationEntities.py
+    - ParsingEntities.py
+"""
