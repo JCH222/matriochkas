9566e8093a494e3b7101bb682ba3be943df93ae8
JCH <jean-charles.hardy@estaca.eu>
2017-11-15 17:22:17 +0100

    Modify unit test

--- matriochkas/tests/Configuration.py
+++ matriochkas/tests/Configuration.py
@@ -10,10 +10,11 @@ def test_stream_class_configuration():
     assert Configuration.StreamClassConfiguration.StringIO.name == 'StringIO'
     assert Configuration.StreamClassConfiguration.StringIO.value == {'read_method': 'read', 'write_method': 'write',
                                                                      'return_method': 'getvalue',
-                                                                     'close_method': 'close'}
+                                                                     'close_method': 'close', 'seek_method': 'seek'}
 
     assert Configuration.StreamClassConfiguration.TextIOWrapper.name == 'TextIOWrapper'
     assert Configuration.StreamClassConfiguration.TextIOWrapper.value == {'read_method': 'read',
                                                                           'write_method': 'write',
                                                                           'return_method': 'None',
-                                                                          'close_method': 'close'}
+                                                                          'close_method': 'close',
+                                                                          'seek_method': 'seek'}
