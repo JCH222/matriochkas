73bd2c72f0a21020921a1de6a027441da17307a0
JCH <jean-charles.hardy@estaca.eu>
2017-12-19 23:57:27 +0100

    Raise additional exception

--- matriochkas/core/IO.py
+++ matriochkas/core/IO.py
@@ -137,7 +137,10 @@ class StreamReader(StreamEntity):
 
         self.run()
         if self._readResult['error'] is None:
-            return self._readResult['parsing_result']
+            if self._readResult['parsing_result'] is not None:
+                return self._readResult['parsing_result']
+            else:
+                raise NotImplementedError('Parsing result is not implemented and no errors are detected')
         else:
             raise self._readResult['error']
 
